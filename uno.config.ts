6b6b628dedf45703e6bd46d9b064ecb3970d6ce4
yourname <example@example.com>
2023-02-12 17:10:58 -0800

    fix: build

--- uno.config.ts
+++ uno.config.ts
@@ -18,7 +18,7 @@ export default defineConfig<Theme>({
   presets: [
     presetUno({
       dark: "class",
-      variablePrefix: false,
+      variablePrefix: "",
     }),
     presetIcons({
       scale: 1.2,
