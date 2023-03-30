c33a7c0acac5c4c8910c7b57a47126eb45f833bc
rehydratedmoon <noemail@lol.com>
2023-03-30 14:32:38 -0700

    fix: add aria expanded

--- src/scripts/sideMenu.ts
+++ src/scripts/sideMenu.ts
@@ -1,11 +1,13 @@
 export const openMenu = () => {
     document.documentElement.style.setProperty('--is-side-menu-close', 'var(--FALSE)');
     document.documentElement.style.setProperty('--is-side-menu-open', 'var(--TRUE)');
+    document.querySelector('#hamburger-button')?.setAttribute('aria-expanded', 'true');
     document.querySelector('#not-sidebar')?.setAttribute('inert', '');
 }
 
 export const closeMenu = () => {
-     document.documentElement.style.setProperty('--is-side-menu-open', 'var(--FALSE)');
-     document.documentElement.style.setProperty('--is-side-menu-close', 'var(--TRUE)');
+    document.documentElement.style.setProperty('--is-side-menu-open', 'var(--FALSE)');
+    document.documentElement.style.setProperty('--is-side-menu-close', 'var(--TRUE)');
+    document.querySelector('#hamburger-button')?.setAttribute('aria-expanded', 'false');
      document.querySelector('#not-sidebar')?.removeAttribute('inert');
 }
