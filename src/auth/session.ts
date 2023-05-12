170f6a285f87f4bc63b7543b2395eed195ff7c9d
rehydratedmoon <noemail@lol.com>
2023-05-12 15:02:38 -0700

    fix: auth

--- src/auth/session.ts
+++ src/auth/session.ts
@@ -1,15 +1,22 @@
 import cookie from 'cookie';
 import supabase from '@db/supabase';
 
-
 export async function getUser(req: Request) {
   const c = cookie.parse(req.headers.get('cookie') ?? '');
   if (!c.sbat) return null;
 
-  const sessionData = await supabase.auth.getSession();
-  const user = sessionData.session.user;
-  if (!user || user.role !== 'authenticated') return null;
+  const {
+    data: { user },
+  } = await supabase.auth.getUser(c.sbat);
+  if (!user || user.role !== 'authenticated') {
+    return null;
+  }
   return user;
+
+  // const sessionData = await supabase.auth.getSession();
+  // const user = sessionData.session.user;
+  // if (!user || user.role !== 'authenticated') return null;
+// return user;
 }
 
 export async function isLoggedIn(req: Request) {
